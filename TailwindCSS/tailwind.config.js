15b7426ab46de3edbd71e1a2982a4736c2c4bc9d
Patricia Molnar <m.patricia@live.com>
2021-09-12 11:24:24 +0100

    adding desktop styles

--- TailwindCSS/tailwind.config.js
+++ TailwindCSS/tailwind.config.js
@@ -13,12 +13,12 @@ module.exports = {
         lime: colors.lime
       },
       backgroundImage: theme => ({
-        'photo-1': "url('https://images.pexels.com/photos/4338015/pexels-photo-4338015.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')", 
-        'photo-2': "url('https://images.pexels.com/photos/7267852/pexels-photo-7267852.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')",
-        'photo-3': "url('https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')", 
-        'photo-4': "url('https://images.pexels.com/photos/4072840/pexels-photo-4072840.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')", 
-        'photo-5': "url('https://images.pexels.com/photos/5709180/pexels-photo-5709180.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')",
-        'photo-6': "url('https://images.pexels.com/photos/2749481/pexels-photo-2749481.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')"
+        'photo-1': "url('https://images.pexels.com/photos/5740842/pexels-photo-5740842.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')", 
+        'photo-2': "url('https://images.pexels.com/photos/5474504/pexels-photo-5474504.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')",
+        'photo-3': "url('https://images.pexels.com/photos/1938345/pexels-photo-1938345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')", 
+        'photo-4': "url('https://images.pexels.com/photos/1819656/pexels-photo-1819656.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')", 
+        'photo-5': "url('https://images.pexels.com/photos/1938348/pexels-photo-1938348.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')",
+        'photo-6': "url('https://images.pexels.com/photos/1674624/pexels-photo-1674624.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')"
       })
     },
   },
