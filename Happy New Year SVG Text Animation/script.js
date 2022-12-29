7de03ddb205624fe174a416046499928ff21d28e
Patricia Molnar <patricia.molnar@universitycompare.com>
2022-12-29 14:26:58 +0000

    [T26] - adding source code for the happy new year animation

---
+++ Happy New Year SVG Text Animation/script.js
@@ -0,0 +1,18 @@
+let hue = 60; 
+let delay = 0; 
+
+const min = 100;
+const max = 400; 
+
+const elements = document.querySelectorAll('svg path'); 
+elements.forEach((element) => {
+    const length = element.getTotalLength();
+    element.style.strokeDashoffset = length;
+    element.style.strokeDasharray = length; 
+    element.style.animationDelay = `${delay}s`; 
+    element.style.stroke = `hsl(${hue}, 90%, 70%)`; 
+    delay += 0.6; 
+    hue += 20; 
+}); 
+
+
